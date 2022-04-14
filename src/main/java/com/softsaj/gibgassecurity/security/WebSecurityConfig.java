575d7c04689b2818bcb1d1b89aafac5e7765ed7b
MarcosGabrielL <marcos_gabrielsaj@hotmail.com>
2022-04-14 14:09:38 -0400

    Update

--- src/main/java/com/softsaj/gibgassecurity/security/WebSecurityConfig.java
+++ src/main/java/com/softsaj/gibgassecurity/security/WebSecurityConfig.java
@@ -78,7 +78,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
         http.cors().and()
 	  .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/islogged/**", "/forgot_password/**","/reset_password/**", "/register", "/*", "/user/**", "/cinefilos/**", "/textoes/**", "/files/**", "/file/**").permitAll()
+                .antMatchers("/vendedores/**","/islogged/**", "/forgot_password/**","/reset_password/**", "/register", "/*", "/user/**", "/cinefilos/**", "/textoes/**", "/files/**", "/file/**").permitAll()
                 .antMatchers("/users").authenticated()
                 .antMatchers("/authenticate")
                 .permitAll().anyRequest().authenticated()
