2b033e050bdf991c778ccdb0ad3f33df25f41327
MarcosGabrielL <marcos_gabrielsaj@hotmail.com>
2022-04-21 17:35:35 -0400

    Update

---
+++ src/main/java/com/softsaj/gibgassecurity/repositories/PerfilPagamentoRepository.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.softsaj.gibgassecurity.repositories;
+
+
+import com.softsaj.gibgassecurity.models.PerfilPagamento;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+import com.softsaj.gibgassecurity.models.PerfilPagamento;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author Marcos
+ */
+@Repository
+public interface PerfilPagamentoRepository extends JpaRepository<PerfilPagamento, Long> {
+    
+     Optional<PerfilPagamento> findPerfilPagamentoById(Long id);
+     
+     void deletePerfilPagamentoById(Long id);
+}
